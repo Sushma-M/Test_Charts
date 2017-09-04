b9ffb32361546a60db2872c225b32e33179f711d
Sushma Wavemaker <sushma.maddirala@wavemaker.com>
2017-09-04 09:12:10 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "gray-orange",
+  "defaultLanguage" : "en",
+  "displayName" : "Test_Charts",
+  "homePage" : "Main",
+  "name" : "Test_Charts",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
